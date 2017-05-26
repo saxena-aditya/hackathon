7400ad66b643ebb6a51c6fd2f105ed785822e6bc
Aditya Saxena <adityasaxena602@gmail.com>
2017-05-26 13:41:02 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -39,50 +39,74 @@ function populate($data){
  ?>
 <div class="container-fluid">
   <div class="row">
-
-              <div class="col-sm-3 col-md-4">
-                 <div class="panel panel-default">
-                    <div class="panel-body"  >
-                      <div style="text-align:center">
-                            <img src="<?=$row['photo_path']?>" alt="users-image"style="width:80%;height:auto;">
-                      </div>
-                      <h4 style="text-align:center"><?=$row['full_name']?><br><small>@<?=$row['college']?></small></h4>
-                      <p>Branch: <small><?=$row['branch']?></small></p>
-                      <p>Sem: <small><?=$row['sem']?></small></p>
-                      <p>Email: <small><?=$row['email']?></small></p>
-                      <form role="form" action="details.php" method="post" enctype="multipart/form-data">
-                            <a type="submit" href="edit_profile" class="btn btn-success pull-left">Edit Profile</a>
-                            <a href="post.php"><button type="button" class="btn btn-success pull-right">New Idea</button></a>
-                      </form>
-                  </div>
-               </div>
-             </div>
-             <div class="col-sm-9 col-md-8">
-             <?php 
-              if ($data){
-                while($f_data = mysqli_fetch_array($sql_recent_result, MYSQLI_ASSOC)){
-
-                  ?>
-                  <div class="panel project-pack panel-default">
-                   <div class="panel-body">
-                     <h4><a href="details.php"><?php echo $f_data['project_title']; ?></a>
-                      <small class="pull-right">End : <?php echo populate($f_data['end']); ?></small>
-
-                       <small class="pull-right">Start : <?php echo $f_data['_begin']; ?> &nbsp;</small>
-                     </h4>
-
-
-                 </div>
-
-                 </div>
-                  <?php
-                }
-              }
-             ?>
-                 <form role="form" action="global.php" method="post" enctype="multipart/form-data">
-                       <button type="submit" class="btn btn-success pull-left">Add Projects</button>
-                 </form>
-              </div>
-            </div>
-     </div>
+    <div class="col-sm-3 col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-body"  >
+          <div style="text-align:center">
+            <img src="<?=$row['photo_path']?>" alt="users-image"style="width:80%;height:auto;">
+          </div>
+          <h4 style="text-align:center">
+            <?=$row['full_name']?>
+            <br>
+            <small>@
+              <?=$row['college']?>
+            </small>
+          </h4>
+          <p>Branch: 
+            <small>
+              <?=$row['branch']?>
+            </small>
+          </p>
+          <p>Sem: 
+            <small>
+              <?=$row['sem']?>
+            </small>
+          </p>
+          <p>Email: 
+            <small>
+              <?=$row['email']?>
+            </small>
+          </p>
+          <form role="form" action="details.php" method="post" enctype="multipart/form-data">
+            <a type="submit" href="edit_profile" class="btn btn-success pull-left">Edit Profile
+            </a>
+            <a href="post.php">
+              <button type="button" class="btn btn-success pull-right">New Idea
+              </button>
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-9 col-md-8">
+      <?php 
+if ($data){
+while($f_data = mysqli_fetch_array($sql_recent_result, MYSQLI_ASSOC)){
+?>
+      <div class="panel project-pack panel-default">
+        <div class="panel-body">
+          <h4>
+            <a href="details.php">
+              <?php echo $f_data['project_title']; ?>
+            </a>
+            <small class="pull-right">End : 
+              <?php echo populate($f_data['end']); ?>
+            </small>
+            <small class="pull-right">Start : 
+              <?php echo $f_data['_begin']; ?> &nbsp;
+            </small>
+          </h4>
+        </div>
+      </div>
+      <?php
+          }
+        }
+      ?>
+      <form role="form" action="global.php" method="post" enctype="multipart/form-data">
+        <button type="submit" class="btn btn-success pull-left">Add Projects
+        </button>
+      </form>
+    </div>
+  </div>
 </div>
+
